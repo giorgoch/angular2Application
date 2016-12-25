6b0bfc1c2aa5cfa51d486b2574777a8403278562
giorgoch <chatziefstratiougeorgios@gmail.com>
2016-12-25 15:37:11 +0200

    finished with tests

--- src/app/todo-data.service.spec.ts
+++ src/app/todo-data.service.spec.ts
@@ -3,6 +3,7 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 import { TodoDataService } from './todo-data.service';
 
+//this  are the tests
 describe('TodoDataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,6 +14,8 @@ describe('TodoDataService', () => {
   it('should ...', inject([TodoDataService], (service: TodoDataService) => {
     expect(service).toBeTruthy();
   }));
+
+  //test for get All Todos
   describe('#getAllTodos()', () => {
     it('should return an empty array by default', inject([TodoDataService], (service: TodoDataService) => {
       expect(service.getAllTodos()).toEqual([]);
@@ -27,12 +30,15 @@ describe('TodoDataService', () => {
 
   });
 
+  //test for save
   describe('#save(todo)', () => {
     it('should automatically assign an incrementing id', inject([TodoDataService], (service: TodoDataService) => {
       let todo1 = new Todo({ title: 'Hello 1 ', complete: false });
       let todo2 = new Todo({ title: 'Hello 2', complete: true });
     }));
   });
+
+  //test for delete todo by id
   describe('#deleteTodoById(id)', () => {
     it('should remove todo with the corresponding id', inject([TodoDataService], (service: TodoDataService) => {
       let todo1 = new Todo({ title: 'Hello 1', complete: false });
@@ -45,6 +51,7 @@ describe('TodoDataService', () => {
     }));
   });
 
+  //test for update
   describe('#updateTodoById(id,values)', () => {
     it('should return todo with the corresponding id and updated data', inject([TodoDataService], (service: TodoDataService) => {
       let todo = new Todo({ title: 'Hello 1', complete: false });
@@ -52,19 +59,20 @@ describe('TodoDataService', () => {
       let updateTodo = service.updateTodoById(1, { title: 'new title' });
       expect(updatedTodo.title).toEqual('new title');
     }));
-    it('should return null if todo is not found',inject([TodoDataService],(service:TodoDataService)=>{
-      let todo = new Todo({title:'Hello 1 ',complete:false});
+    it('should return null if todo is not found', inject([TodoDataService], (service: TodoDataService) => {
+      let todo = new Todo({ title: 'Hello 1 ', complete: false });
       service.addTodo(todo);
-      let updatedTodo(=service.updateTodoById(2,{title:'new title'}));
-      expect(updatedTodo)=service.toEqual(null);
+      let updatedTodo(=service.updateTodoById(2, { title: 'new title' }));
+      expect(updatedTodo) = service.toEqual(null);
     }));
   });
 
-  describe('#toogleTodoComplete(todo)',()=>{
-    it('should return the updated todo with inverse complete status',inject([TodoDataService],(service:TodoDataService)=>{
-      let todo = new Todo({title:'Hello 1',complete:false});
+  //test for toogle
+  describe('#toogleTodoComplete(todo)', () => {
+    it('should return the updated todo with inverse complete status', inject([TodoDataService], (service: TodoDataService) => {
+      let todo = new Todo({ title: 'Hello 1', complete: false });
       service.addTodo(todo);
-      let updatedTodo= service.toggleTodoComplete(todo);
+      let updatedTodo = service.toggleTodoComplete(todo);
       expect(updatedTodo.complete).toEqual(true);
       service.toggleTodoComplete(todo);
       expect(updatedTodo.complete).toEqual(false);
